d5d477bfb082ab9963b2370e3fd61f8fcf0411a8
zorazky <oswaldo.vazquez@gmail.com>
2020-05-06 22:33:01 -0500

    showing data

--- scripts/index.js
+++ scripts/index.js
@@ -2,16 +2,36 @@ const API_KEY = `DEMO_KEY`;
 const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
-
 const previousWeather = document.querySelector('.previous-weather');
-
+const currentSolElement = document.querySelector('[data-current-sol]');
+const currentDateElement = document.querySelector('[data-current-date]');
+const currentTempHighElement = document.querySelector('[data-current-temp-high]');
+const currentTempLowElement = document.querySelector('[data-current-temp-low]');
+const windSpeedElement = document.querySelector('[data-wind-speed]');
+const windDirectionText = document.querySelector('[data-wind-direction-text]');
+const windDirectionArrow = document.querySelector('[data-wind-direction-arrow]');
+ 
 previousWeatherToggle.addEventListener('click', () => {
     previousWeather.classList.toggle('show-weather');
 } )
 
+let selectedSolIndex;
+
 getWeather().then(sols => {
-    console.log(sols);
+    selectedSolIndex = sols.length - 1;
+    displaySelectedSol(sols);
 });
+
+function displaySelectedSol(sols) {
+    const selectedSol = sols[selectedSolIndex];
+    currentSolElement.innerText = selectedSol.sol;
+    currentDateElement.innerText = selectedSol.date;
+    currentTempHighElement.innerText = selectedSol.maxTemp;
+    currentTempLowElement.innerText = selectedSol.minTemp;
+    windSpeedElement.innerText = selectedSol.windSpeed;
+    windDirectionArrow.getElementsByClassName.setProperty('--direction', `${selectedSol.windDirectionDegrees}deg`);
+    windDirectionText = selectedSol.windDirectionCardinal;
+}
 
 function getWeather() {
     return fetch(API_URL)
