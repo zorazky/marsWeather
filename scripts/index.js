704985e5cb55d03500b24e756eb459943cbd53c6
zorazky <oswaldo.vazquez@gmail.com>
2020-05-06 22:02:48 -0500

    API conected

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+const API_KEY = `DEMO_KEY`;
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
 
 const previousWeather = document.querySelector('.previous-weather');
@@ -5,3 +8,30 @@ const previousWeather = document.querySelector('.previous-weather');
 previousWeatherToggle.addEventListener('click', () => {
     previousWeather.classList.toggle('show-weather');
 } )
+
+getWeather().then(sols => {
+    console.log(sols);
+});
+
+function getWeather() {
+    return fetch(API_URL)
+        .then(res => res.json())
+        .then(data => {
+            const {
+                sol_keys,
+                validity_checks,
+                ...solData
+            } = data
+            return Object.entries(solData).map(([sol, data]) => {
+                return {
+                    sol: sol,
+                    maxTemp: data.AT.mx,
+                    minTemp: data.AT.mn,
+                    windSpeed: data.HWS.av,
+                    windDirectionDegrees: data.WD.most_common.compass_degrees,
+                    windDirectionCardinal: data.WD.most_common.compass_point,
+                    date: new Date(data.First_UTC)
+                }
+            })
+        })
+}
