d3014195e8fc2d8349ab6e8f390eb1ae0a4d2a46
zorazky <oswaldo.vazquez@gmail.com>
2020-05-07 11:09:28 -0500

    some bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,78 @@
-const API_KEY = `DEMO_KEY`;
-const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`;
+const API_KEY = 'DEMO_KEY'
+const API_URL = `https://api.nasa.gov/insight_weather/?api_key=${API_KEY}&feedtype=json&ver=1.0`
 
 const previousWeatherToggle = document.querySelector('.show-previous-weather');
-const previousWeather = document.querySelector('.previous-weather');
-const currentSolElement = document.querySelector('[data-current-sol]');
-const currentDateElement = document.querySelector('[data-current-date]');
-const currentTempHighElement = document.querySelector('[data-current-temp-high]');
-const currentTempLowElement = document.querySelector('[data-current-temp-low]');
-const windSpeedElement = document.querySelector('[data-wind-speed]');
-const windDirectionText = document.querySelector('[data-wind-direction-text]');
-const windDirectionArrow = document.querySelector('[data-wind-direction-arrow]');
+const previousWeather = document.querySelector('.previous-weather')
+
+const currentSolElement = document.querySelector('[data-current-sol]')
+const currentDateElement = document.querySelector('[data-current-date]')
+const currentTempHighElement = document.querySelector('[data-current-temp-high]')
+const currentTempLowElement = document.querySelector('[data-current-temp-low]')
+const windSpeedElement = document.querySelector('[data-wind-speed]')
+const windDirectionText = document.querySelector('[data-wind-direction-text]')
+const windDirectionArrow = document.querySelector('[data-wind-direction-arrow]')
+
+const previousSolTemplate = document.querySelector('[data-previous-sol-template]')
+const previousSolContainer = document.querySelector('[data-previous-sols]')
+
+const unitToggle = document.querySelector('[data-unit-toggle]')
+const metricRadio = document.getElementById('cel')
+const imperialRadio = document.getElementById('fah')
  
 previousWeatherToggle.addEventListener('click', () => {
-    previousWeather.classList.toggle('show-weather');
-} )
+	previousWeather.classList.toggle('show-weather')
+})
 
 let selectedSolIndex;
 
 getWeather().then(sols => {
     selectedSolIndex = sols.length - 1;
     displaySelectedSol(sols);
+    displayPreviousSols(sols);
 });
 
 function displaySelectedSol(sols) {
     const selectedSol = sols[selectedSolIndex];
     currentSolElement.innerText = selectedSol.sol;
-    currentDateElement.innerText = selectedSol.date;
-    currentTempHighElement.innerText = selectedSol.maxTemp;
-    currentTempLowElement.innerText = selectedSol.minTemp;
-    windSpeedElement.innerText = selectedSol.windSpeed;
-    windDirectionArrow.getElementsByClassName.setProperty('--direction', `${selectedSol.windDirectionDegrees}deg`);
-    windDirectionText = selectedSol.windDirectionCardinal;
+    currentDateElement.innerText = displayDate(selectedSol.date);
+    currentTempHighElement.innerText = displayTemperature(selectedSol.maxTemp);
+    currentTempLowElement.innerText = displayTemperature(selectedSol.minTemp);
+    windSpeedElement.innerText = displaySpeed(selectedSol.windSpeed);
+    windDirectionArrow.style.setProperty('--direction', `${selectedSol.windDirectionDegrees}deg`);
+    windDirectionText.innerText = selectedSol.windDirectionCardinal
+}
+
+function displayPreviousSols(sols) {
+    previousSolContainer.innerHTML = '';
+    sols.forEach((solData, index) => {
+        const solContainer = previousSolTemplate.content.cloneNode(true);
+        solContainer.querySelector('[data-sol]').innerText = solData.sol;
+        solContainer.querySelector('[data-date]').innerText = displayDate(solData.date);
+        solContainer.querySelector('[data-temp-high]').innerText = displayTemperature(solData.maxTemp);
+        solContainer.querySelector('[data-temp-low]').innerText = displayTemperature(solData.minTemp);
+        solContainer.querySelector('[data-select-button]').addEventListener('click', () => {
+            selectedSolIndex = index;
+            displaySelectedSol(sols);
+        })
+        previousSolContainer.appendChild(solContainer);
+    })
+}
+
+
+
+function displayTemperature(temperature) {
+    return Math.round(temperature);
+}
+
+function displaySpeed(speed) {
+    return Math.round(speed);
+}
+
+function displayDate(date) {
+    return date.toLocaleDateString(
+        undefined,
+        { day: 'numeric', month: 'long'}
+    )
 }
 
 function getWeather() {
